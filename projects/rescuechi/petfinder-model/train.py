66d396932a401c119405d825f75f769d8b2979dd
Kayla Robinson <kaylarobinson077@gmail.com>
2022-11-09 18:43:01 -0600

    sketch out funcs a bit more

--- projects/rescuechi/petfinder-model/train.py
+++ projects/rescuechi/petfinder-model/train.py
@@ -2,18 +2,27 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+import pickle
 
 def load_data():
+    
+    # TODO read in actual features
     data = pd.read_csv()
     X_train, X_test, y_train, y_test = train_test_split(data, test_size=0.2, random_state=312)
+    
     return X_train, X_test, y_train, y_test
 
 def train(X_train, y_train):
+    
     clf = RandomForestRegressor(n_estimators=1000, random_state=312)
     clf.fit(X_train, y_train)
+    
+    pickle.dump(clf, open("model.pkl", "wb"))
+    
     return clf
     
 def evaluate(clf, X_test, y_test):
+    
     yhat = clf.predict(X_test)
     mse = metrics.mean_squared_error(y_test, yhat)
     mae = metrics.mean_absolute_error(y_test, yhat)
@@ -29,6 +38,7 @@ def evaluate(clf, X_test, y_test):
 
 
 def main():
+    
     X_train, X_test, y_train, y_test = load_data()
     clf = train(X_train, y_train)
     metrics = evaluate(clf, X_test, y_test)
